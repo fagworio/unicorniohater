db002484a1420912fb580021863746b944a323d3
fagworio <fagworio@gmail.com>
2023-11-03 11:46:43 -0300

    Moved to the sibebars directory for better organization

---
+++ template-parts/sidebars/right-sidebar.php
@@ -0,0 +1,7 @@
+<?php if (is_active_sidebar('right-sidebar')) : ?>
+    <aside>
+        <div class="sidebar">
+            <?php dynamic_sidebar('right-sidebar'); ?>
+        </div>
+    </aside>
+<?php endif; ?>
---
+++ template-parts/sidebars/sidebar-search.php
@@ -0,0 +1,5 @@
+<?php if (is_active_sidebar('search-page-right-sidebar')) : ?>
+    <aside class="right-sidebar">
+        <?php dynamic_sidebar('search-page-right-sidebar'); ?>
+    </aside>
+<?php endif; ?>
