e14b9adb1f220c36ef42db61fd5415d61680452f
fagworio <fagworio@gmail.com>
2023-11-03 11:49:28 -0300

    Add loop to index page

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
-/**
- * The main template file
- */
+
+get_header();
+get_critical_css('page.min.css');
+
+while ( have_posts() ) :
+	the_post();
+	get_template_part( 'template-parts/content-page' );
+
+	// If comments are open or there is at least one comment, load up the comment template.
+	if ( comments_open() || get_comments_number() ) {
+		comments_template();
+	}
+
+    endwhile; // End of the loop.
+
+get_footer();
